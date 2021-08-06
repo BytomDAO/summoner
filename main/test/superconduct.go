36f71f8328728167cb7872157e0ffc09487ca5df
hyl <563807243@qq.com>
2021-08-06 11:57:20 +0800

    adjust global var

--- main/test/superconduct.go
+++ main/test/superconduct.go
@@ -1,8 +1,10 @@
 // 三个资产类型全局常量，可以先用全局变量实现
 // asset资产类型，32个字节长度的资产
-assetX := asset(1)  // int类型强转
-assetY := asset(2) 
-assetM := asset(3) 
+var (
+	assetX = asset(1) // int类型强转
+	assetY = asset(2)
+	assetM = asset(3)
+)
 
 // struct结构描述合约结构：field为三资产池状态数据，function为存取兑解锁函数
 // 暂通过全局函数实现
@@ -16,24 +18,24 @@ assetM := asset(3)
 // 状态数据传入参数，三资产池资产数量state [poolAmtX,poolAmtY,poolAmtM]
 // 注意这里传参summoner的处理过程
 //     如：[][]byte{vm.Int64Bytes(10), vm.Int64Bytes(100), vm.Int64Bytes(1000)}
-func Swap(amtX amount) {  // 资产数量amount无符号整型，可为uint64类型别名
+func Swap(amtX amount) { // 资产数量amount无符号整型，可为uint64类型别名
 	verify(amtX > 0) // 暂builtin实现
-	verify(poolAmtX * poolAmtY == poolAmtM)
-  
-	newPoolAmtX := poolAmtX + amtX   // 暂不计算手续费
+	verify(poolAmtX*poolAmtY == poolAmtM)
+
+	newPoolAmtX := poolAmtX + amtX // 暂不计算手续费
 	newPoolAmtY := poolAmtM / newPoolAmtX
 
 	lock(newPoolAmtX, assetX, program, state) // 注：state经过summoner取算存的过程
-	lock(newPoolAmtY, assetY, program, state) 
-	lock(poolAmtM, assetM, program, state) 
+	lock(newPoolAmtY, assetY, program, state)
+	lock(poolAmtM, assetM, program, state)
 }
 
 // TOOD
-func  AddLiquidity(amtX amount, amtY amount) {
+func AddLiquidity(amtX amount, amtY amount) {
 }
 
 // TOOD
-func  RemoveLiquidity(amtX amount, amtY amount) {
+func RemoveLiquidity(amtX amount, amtY amount) {
 }
 
 
